c5385496cb272f3ffd0188765a84c7fcb89ec14f
Romuald Lewandoski <romuald.lewandoski@yandex.com>
2021-01-04 13:43:21 +0100

    Finish front & back without ajax

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Todo;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api/listAll", name="list_all")
+     */
+    public function listAll(): JsonResponse
+    {
+        $obj = new \stdClass();
+        $em = $this->getDoctrine()->getManager();
+        $todoRepo = $em->getRepository(Todo::class);
+        $todoLists = $todoRepo->findAll();
+        //$obj->todos = $todoLists;
+        $arr = [];
+        foreach ($todoLists as $todo) {
+            $temp = new \stdClass();
+            $temp->id = $todo->getId();
+            $temp->action = $todo->getAction();
+            $temp->isDone = $todo->getIsDone();
+            array_push($arr, $temp);
+        }
+        $obj->todos = $arr;
+        /*
+         * {
+         *  todos: [
+         *      {
+         *          id: 1,
+         *          action: "Allumez le pc",
+         *          isDone: false
+         *      },{
+         *          id : 2 ....
+         *      }
+         *     ]
+         */
+        return new JsonResponse($obj);
+    }
+
+    /**
+     * @Route("/api/finish/{id}", name="finish_todo")
+     */
+    public function finishTodo(Todo $todo)
+    {
+        $obj = new \stdClass();
+
+        if ($todo == null) {
+            $obj->status = 404;
+        } else {
+            $obj->status = 200;
+            if ($todo->getIsDone()) {
+                $todo->setIsDone(false);
+            } else {
+                $todo->setIsDone(true);
+            }
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+        }
+
+        return new JsonResponse($obj);
+    }
+
+    /**
+     * @Route("/api/new/", name="new_todo", methods={"POST"})
+     * @param Request $request
+     */
+    public function addTodo(Request $request)
+    {
+        $obj = new \stdClass();
+        $action = $request->request->get('action');
+        if ($action == null || $action == "") {
+            $obj->status = 500;
+            $obj->error = "Champs manquants";
+        } else {
+            $todo = new Todo();
+            $todo->setAction($action);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todo);
+            $em->flush();
+            $obj->status = 200;
+            $obj->id = $todo->getId();
+        }
+
+        return new JsonResponse($obj);
+    }
+
+
+    /**
+     * @Route("/api/delete/{id}", name="delete_todo")
+     * @param Todo $todo
+     */
+    public function removeTodo(Todo $todo)
+    {
+        $obj = new \stdClass();
+
+        if ($todo == null) {
+            $obj->status = 404;
+        } else {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($todo);
+            $em->flush();
+            $obj->status = 200;
+        }
+
+        return new JsonResponse($obj);
+    }
+}
---
+++ src/Entity/Todo.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $action;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isDone;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAction(): ?string
+    {
+        return $this->action;
+    }
+
+    public function setAction(string $action): self
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    public function getIsDone(): ?bool
+    {
+        return $this->isDone;
+    }
+
+    public function setIsDone(bool $isDone): self
+    {
+        $this->isDone = $isDone;
+
+        return $this;
+    }
+}
